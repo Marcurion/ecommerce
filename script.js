1394d4ec9d170c6f3556c9bdc4668fa92d78ec2a
Marcurion <marcurion@private>
2023-12-04 10:55:57 +0100

    snipcart integrated, end of section 3

--- script.js
+++ script.js
@@ -5,4 +5,16 @@ window.onload = () => {
         behavior: "smooth"
       });
     });
+
+    const quantities = Array.from(document.getElementsByClassName("quantity"));
+    quantities.forEach(element => { element.addEventListener("change", (e) => {
+        console.log(e.target.value);
+        const product = element.closest(".product");
+        if (product)
+        {
+            product.getElementsByClassName("snipcart-add-item")?.[0]?.setAttribute("data-item-quantity", e.target.value);
+        }
+    })
+        
+    });
   };
